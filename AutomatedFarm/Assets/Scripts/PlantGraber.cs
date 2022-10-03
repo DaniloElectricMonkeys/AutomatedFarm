15e0c3cd20d3a173dd0935e6d3c325dd63559cdb
RenanPBet <renan.bet@electricmonkeys.games>
2022-10-03 19:05:47 -0300

    ADDED machine to collect plants. Organization

---
+++ AutomatedFarm/Assets/Scripts/PlantGraber.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGraber : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float influenceArea = 2;
+    public LayerMask plantLayerMask;
+    List<Collider> plantsHit = new List<Collider>();
+    List<Collider> cachedPlants = new List<Collider>();
+    
+
+    private void Awake() {
+        PlantGrow.OnPlantReady += CollectPlant;
+        PlantGrow.OnPlantPlaced += AssignPlants;
+    }
+
+    private void Start() {
+        AssignPlants();
+    }
+
+    private void CollectPlant(GameObject plant) {
+        foreach (var item in cachedPlants)
+            item.GetComponent<PlantGrow>()?.Harvest();
+    }
+
+    void AssignPlants(){
+        plantsHit = Physics.OverlapBox(transform.position, ((Vector3.one * influenceArea)/2),Quaternion.identity, plantLayerMask).ToList();
+
+        if(plantsHit.Count <= 0) return;
+
+        //Remove plants that are arealdy assigned to a graber
+        foreach (var item in plantsHit)
+        {
+            PlantGrow plant = item.GetComponent<PlantGrow>();
+            if(plant.isAssignedToGraber == false) cachedPlants.Add(item);
+        }
+
+        foreach (var item in cachedPlants)
+            item.GetComponent<PlantGrow>()?.Harvest();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, Vector3.one * influenceArea);
+    }
+}
