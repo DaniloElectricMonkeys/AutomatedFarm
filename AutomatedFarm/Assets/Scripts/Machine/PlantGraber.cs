00f616bb27e7eccffa6e29b3f8ad2e0771fb7d85
RenanPBet <renan.bet@electricmonkeys.games>
2022-10-24 12:53:47 -0300

    FIXED Drone Harvest() now interact with soil state.

--- AutomatedFarm/Assets/Scripts/Machine/PlantGraber.cs
+++ AutomatedFarm/Assets/Scripts/Machine/PlantGraber.cs
@@ -4,61 +4,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlantGraber : OutputMachine
+namespace AutomatedFarm
 {
-    [Space]
-    [Header("Collection Area")]
-    public Vector3 boxSize = new Vector3(10,3,10);
-    public Vector3 boxOffset;
-    public LayerMask plantLayerMask;
-    List<Collider> plantsHit = new List<Collider>();
-    protected List<Collider> cachedPlants = new List<Collider>();
-
-    protected virtual void Awake() {
-        PlantGrow.OnPlantReady += CollectPlant;
-        PlantGrow.OnPlantPlaced += AssignPlants;
-    }
-
-    protected virtual void OnDestroy() {
-        PlantGrow.OnPlantReady -= CollectPlant;
-        PlantGrow.OnPlantPlaced -= AssignPlants;
-    }
-
-    private void Start() {
-        AssignPlants();
-    }
-
-    protected virtual void CollectPlant(GameObject plant) {
-        foreach (var item in cachedPlants)
-            if(item.GetComponent<PlantGrow>()?.Harvest() == true)
-                resourceAmount++;
-    }
-
-    protected virtual void AssignPlants()
+    public class PlantGraber : OutputMachine
     {
-        plantsHit = Physics.OverlapBox(transform.position, boxSize / 2, Quaternion.identity, plantLayerMask).ToList();
+        [Space]
+        [Header("Collection Area")]
+        public Vector3 boxSize = new Vector3(10,3,10);
+        public Vector3 boxOffset;
+        public LayerMask plantLayerMask;
+        List<Collider> plantsHit = new List<Collider>();
+        protected List<Collider> cachedPlants = new List<Collider>();
 
-        if (plantsHit.Count <= 0) return;
-
-        if(cachedPlants.Count > 0)
-            cachedPlants.Clear();
-        //Remove plants that are arealdy assigned to a graber
-        foreach (var item in plantsHit)
-        {
-            PlantGrow plant = item.GetComponent<PlantGrow>();
-                cachedPlants.Add(item);
+        protected virtual void Awake() {
+            PlantGrow.OnPlantReady += CollectPlant;
+            PlantGrow.OnPlantPlaced += AssignPlants;
         }
 
-        AskForCollection();
+        protected virtual void OnDestroy() {
+            PlantGrow.OnPlantReady -= CollectPlant;
+            PlantGrow.OnPlantPlaced -= AssignPlants;
+        }
+
+        private void Start() {
+            AssignPlants();
+        }
+
+        protected virtual void CollectPlant(GameObject plant) {
+            foreach (var item in cachedPlants)
+                if(item.GetComponent<PlantGrow>()?.Harvest() == true)
+                    resourceAmount++;
+        }
+
+        protected virtual void AssignPlants()
+        {
+            plantsHit = Physics.OverlapBox(transform.position, boxSize / 2, Quaternion.identity, plantLayerMask).ToList();
+
+            if (plantsHit.Count <= 0) return;
+
+            if(cachedPlants.Count > 0)
+                cachedPlants.Clear();
+            //Remove plants that are arealdy assigned to a graber
+            foreach (var item in plantsHit)
+            {
+                PlantGrow plant = item.GetComponent<PlantGrow>();
+                    cachedPlants.Add(item);
+            }
+
+            AskForCollection();
+        }
+
+        protected virtual void AskForCollection()
+        {
+            foreach (var item in cachedPlants)
+                CollectPlant(item.gameObject);
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.DrawWireCube(transform.position, boxSize);
+        }
     }
 
-    protected virtual void AskForCollection()
-    {
-        foreach (var item in cachedPlants)
-            CollectPlant(item.gameObject);
-    }
-
-    private void OnDrawGizmos() {
-        Gizmos.DrawWireCube(transform.position, boxSize);
-    }
 }
+
--- AutomatedFarm/Assets/Scripts/PlantGrow.cs
+++ AutomatedFarm/Assets/Scripts/PlantGrow.cs
@@ -4,88 +4,106 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-///<summary>
-/// Grow plants. Simple
-///</summary>
-public class PlantGrow : MonoBehaviour
+namespace AutomatedFarm
 {
-    [Header("Options")]
-    string myName;
-    float time = 11;
-    public ResourceType type;
-    Seed seedSettings;
-    public bool canBeHarvested { get; private set; }    
-    public static Action<GameObject> OnPlantReady;
-    public static Action OnPlantPlaced;
-    public bool ignore;
-    public bool istarget;
-
-    [HideInInspector] public bool isAssignedToGraber;
-
-    private void Start()
+    ///<summary>
+    /// Grow plants. Simple
+    ///</summary>
+    public class PlantGrow : MonoBehaviour
     {
-        if(ignore) return;
-        OnPlantPlaced?.Invoke();
+        [Header("Options")]
+        public LayerMask soilLayer;
+        string myName;
+        float time = 11;
+        public ResourceType type;
+        Seed seedSettings;
+        public bool canBeHarvested { get; private set; }    
+        public static Action<GameObject> OnPlantReady;
+        public static Action OnPlantPlaced;
+        public bool ignore;
+        public bool istarget;
+        public S_SoilState currentSoil;
+        
 
-        if (Library.Instance.GetType(type) != null)
+        [HideInInspector] public bool isAssignedToGraber;
+
+        private void Start()
         {
-            seedSettings = Library.Instance.GetType(type);
-            myName = seedSettings.name;
-            time = seedSettings.timeToGrow;
-        }        
+            if(ignore) return;
+            OnPlantPlaced?.Invoke();
 
-        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-        transform.DOScale(new Vector3(0.33f, 0.33f, 0.33f), 1).SetEase(Ease.InOutBack);
+            if (Library.Instance.GetType(type) != null)
+            {
+                seedSettings = Library.Instance.GetType(type);
+                myName = seedSettings.name;
+                time = seedSettings.timeToGrow;
+            }        
 
-        //transform.DOScale(Vector3.one, time).SetEase(Ease.Linear).OnComplete((CompletePlant));
-        //LeanTween.scale(gameObject, desiredScale, time);
+            transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            transform.DOScale(new Vector3(0.33f, 0.33f, 0.33f), 1).SetEase(Ease.InOutBack);
 
-        StartCoroutine(Grow());
-    }
+            //transform.DOScale(Vector3.one, time).SetEase(Ease.Linear).OnComplete((CompletePlant));
+            //LeanTween.scale(gameObject, desiredScale, time);
 
-    IEnumerator Grow()
-    {
-        float timeLeft = time;
+            Ray ray = new Ray(transform.position + new Vector3(0,3,0), Vector3.down);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 10f, soilLayer))
+            {
+                currentSoil = hit.collider.gameObject?.GetComponent<S_SoilState>();
+            }
 
-        for (int i = 0; i < time / 2; i++)
-        {
-            if (timeLeft <= 0) break;
-            timeLeft--;
-            yield return new WaitForSeconds(1);
+            StartCoroutine(Grow());
         }
 
-        transform.DOScale(new Vector3(0.66f, 0.66f, 0.66f), 1).SetEase(Ease.InOutBack);
-
-        for (int i = 0; i < time / 2; i++)
+        IEnumerator Grow()
         {
-            if (timeLeft <= 0) break;
-            timeLeft--;
-            yield return new WaitForSeconds(1);
+            float timeLeft = time;
+
+            for (int i = 0; i < time / 2; i++)
+            {
+                if (timeLeft <= 0) break;
+                timeLeft--;
+                yield return new WaitForSeconds(1);
+            }
+
+            transform.DOScale(new Vector3(0.66f, 0.66f, 0.66f), 1).SetEase(Ease.InOutBack);
+
+            for (int i = 0; i < time / 2; i++)
+            {
+                if (timeLeft <= 0) break;
+                timeLeft--;
+                yield return new WaitForSeconds(1);
+            }
+
+            transform.DOScale(Vector3.one, 1).SetEase(Ease.InOutBack).OnComplete((CompletePlant));
         }
 
-        transform.DOScale(Vector3.one, 1).SetEase(Ease.InOutBack).OnComplete((CompletePlant));
-    }
-
-    void CompletePlant()
-    {
-        if(ignore) return;
-        canBeHarvested = true;
-        OnPlantReady?.Invoke(gameObject);
-    }
-
-    public void AssignToGraber() => isAssignedToGraber = true;
-
-    public bool Harvest()
-    {
-        if(ignore) return false;
-
-        if (canBeHarvested)
+        void CompletePlant()
         {
-            Destroy(gameObject);
-            /*transform.localScale = Vector3.zero;
-            canBeHarvested = false;*/
-            return true;
+            if(ignore) return;
+            canBeHarvested = true;
+            OnPlantReady?.Invoke(gameObject);
         }
-        return false;
+
+        public void AssignToGraber() => isAssignedToGraber = true;
+
+        public bool Harvest()
+        {
+            if(ignore) return false;
+
+            if (canBeHarvested)
+            {
+                if(currentSoil!= null)
+                    currentSoil.ChangeStateToFree();
+                    
+                Destroy(gameObject);
+                /*transform.localScale = Vector3.zero;
+                canBeHarvested = false;*/
+                return true;
+            }
+            return false;
+        }
     }
 }
+
+
